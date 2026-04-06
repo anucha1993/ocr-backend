e1f8ac62fc185c04c0b3f8c8367b3a0563a1b4c4
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2026-04-06 17:23:52 +0700

    ฟหกหฟก

---
+++ database/migrations/2026_04_06_094703_make_lastname_nullable_in_labours_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('labours', function (Blueprint $table) {
+            $table->string('lastname', 255)->nullable()->default('')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('labours', function (Blueprint $table) {
+            $table->string('lastname', 255)->nullable(false)->default(null)->change();
+        });
+    }
+};
